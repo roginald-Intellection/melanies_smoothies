797f861498a0c0bae21dda4b26fabb64fdea2c1a
roginald-Intellection <roginald.delacruz@intellection.ph>
2025-01-03 10:51:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,13 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
 # st.dataframe(data = my_dataframe, use_container_width = True)
+
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
@@ -27,8 +32,12 @@ ingredients_string = ''
 
 for each_fruit in ingredients_list:
     ingredients_string += (each_fruit + ' ')
+
+    search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+
+    
     st.subheader(each_fruit + 'Nutrition Information')
-    smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{each_fruit}")
+    smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
 st.write(ingredients_string)
